0464fcce78181d6d5048c7423d5665fa216cf0b1
Siva Dasetty <tg825722@c197-112.frontera.tacc.utexas.edu>
2023-07-20 07:42:03 -0500

    added helper script to Nicks' parameterize switching function

---
+++ utils/parameterize_switching_functions.py
@@ -0,0 +1,105 @@
+#module load python/anaconda-2021.05
+#source activate torch-ml
+
+import torch
+import torch.nn as nn
+import numpy as np
+import math
+import re, shutil, tempfile
+import sys
+import os
+import mdtraj as md
+from sklearn.decomposition import PCA
+import subprocess
+
+#inputPIV = np.load("nSFP_NaClpiv61.npy")
+if len(sys.argv) < 2:
+        print("COMMAND LINE ARGS:\n")
+        print("1. Path to xtc file\n")
+        print("2. Path to gro file\n")
+        print("Remember to update atom list in script\n")
+        sys.exit("Too few command line args")
+dcd_file = sys.argv[2]
+pdb_file = sys.argv[1]
+
+#filepath = '/work2/03273/tg825722/shared-folder-siva/doe/hydrolysis/mlcolvar'
+trj = md.load_dcd(dcd_file, top=pdb_file)
+
+
+#chignolin alpha carbons
+#atom_list = ["index 5", "index 26", "index 47", "index 67", "index 73", "index 88", "index 102", "index 109", "index 123", "index 147", "name OW"]
+
+#chignolin hydrogen bonds
+#atom_list = ["index ", "name OW"]
+
+
+#atom_list = ["index 20", "index 17", "index 16", "index 54", "index 52", "index 49", "index 214", "index 212", "index 209", "index 180", "index 177", "index 176"]
+#DNA hairpin
+#atom_list = ["index 16", "index 17", "index 20", "index 49", "index 52", "index 54", "index 79", "index 80", "index 83", "index 112", "index 115", "index 144", "index 147", "index 176", "index 177", "index 180", "index 209", "index 212", "index 214", "name OW"]
+
+# hydrolysis
+atom_name_list = ['O1', 'O2', r'P$_{\alpha}$',  r'P$_{\beta}$', r'P$_{\gamma}$', r'O$_{\beta_{1}}$', r'O$_{\beta_{2}}$', r'O$_{\beta_{3}}$', r'O$_{\gamma_{1}}$', r'O$_{\gamma_{2}}$', r'O$_{\gamma_{3}}$']
+atom_list = [3893, 3894, 13505, 13509, 13513, 13510, 13511, 13512, 13514, 13515, 13516]
+atom_list = ['index ' + str(x-1) for x in atom_list]
+
+
+interactions = []
+for i in range(len(atom_list)-1):
+    for j in range(i+1,len(atom_list)):
+        interactions.append(atom_list[i]+" - "+atom_list[j])
+
+starts = []
+ends = []
+r0s = []
+for i in range(len(atom_list)-1):
+    for j in range(i+1,len(atom_list)):
+        atom_pairs = trj.top.select_pairs(atom_list[i], atom_list[j])
+        dist = md.compute_distances(trj, atom_pairs)
+        bins, counts = md.compute_rdf(trj, atom_pairs, (dist.min(), dist.max()), n_bins=500)
+        #bins, counts = md.compute_rdf(trj, atom_pairs, (0.1, 2.5), n_bins=1000)
+        
+        
+        pk1_start = np.where(counts != 0)[0][0]
+        pk2_end = np.where(counts != 0)[0][-1]
+        r0 = (bins[pk1_start] + bins[pk2_end])/2.0
+        starts.append(bins[pk1_start])
+        ends.append(bins[pk2_end])
+        r0s.append(round(r0,3))
+
+def rat_switch(r, r0, d0, n, m):
+    s = (1 - ((r-d0)/r0)**n)/(1 - ((r-d0)/r0)**m)
+    return s
+
+vec_switch = np.vectorize(rat_switch)
+
+n_vals = np.zeros(len(interactions))
+for n in range(1,100):
+    m = 2*n
+
+    closest_val = vec_switch(starts, r0s, 0, n, m) #input peak 1 start as r
+    pk2_end = vec_switch(ends, r0s, 0, n, m) #input peak 2 end as r
+    for i in range(len(starts)):
+        if closest_val[i] > 0.9 and pk2_end[i] < 0.1:
+            if n_vals[i] == 0:
+                n_vals[i] = n
+
+final_params = []
+for i in range(len(interactions)):
+    final_params.append([r0s[i], n_vals[i], interactions[i]])
+
+r0s = np.asarray(r0s)
+n_vals = np.asarray(n_vals)
+string = ''
+
+for i in range(len(r0s)):
+    string += "SWITCH%d={RATIONAL R_0=%.3f MM=%d NN=%d}\n" %(i+1, r0s[i], 2*n_vals[i], n_vals[i])
+    #print("SWITCH%d={RATIONAL R_0=%.3f MM=%d NN=%d}" %(i+1, r0s[i], 2*n_vals[i], n_vals[i]))
+
+#print(string)
+
+f = open("switching_function_parameters.dat", "w")
+f.write(string)
+#f.write("\n\n\n")
+#for i in interactions:
+#    f.write(str(i) + "\n")
+f.close()
